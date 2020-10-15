f5654b79fc0393c0060c578af3fb7842664db562
二氢茉莉酮酸甲酯 <gao1021229073@live.com>
2020-10-15 22:27:38 +0800

    UPDATE 202010152227
    Update another pages

--- js/login.js
+++ js/login.js
@@ -7,4 +7,14 @@ function logBtnClick()
 		window.open('p//heu//20201014//20201014.html');
 		window.close();
 	}
+	else if(target == "hlp.list.all")
+	{
+		window.open("p//list.html");
+		window.close();
+	}
+	else if(target == "rand.rand.rand")
+	{
+		window.open('p//rand//rand.html');
+		window.close();
+	}
 } 
